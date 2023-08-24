7131dbfb1a86a9c5a2fefe4e2a906dfe11425b95
Mert Önder <mert.ondeeer77@gmail.com>
2023-08-24 17:37:35 +0300

    App.js
    .

--- App.js
+++ App.js
@@ -1,3 +1,30 @@
-if(1==1){
+    // * * * * * *
+    //   * * * *
+    //    * * *
+    //     * *
+    //      *
+    //      *
+    //     * *
+    //    * * *
+    //   * * * *
+    //  * * * * * 
+
+
+    let n = 5; 
+
+
+    for (let i = 0; i < n; i++) {
+        let bosluk = " ".repeat(i);
+        let yildiz = "* ".repeat(n - i);
+        console.log(bosluk + yildiz);
+    }
     
-}
+    
+    for (let i = n; i >= 0; i--) {
+        let bosluk = " ".repeat(i);
+        let yildiz = "* ".repeat(n - i);
+        console.log(bosluk + yildiz);
+    }
+    
+    
+    

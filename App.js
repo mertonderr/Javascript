c7ecfadd66e050221eb95214b090c24c46f3a7de
Mert Önder <mert.ondeeer77@gmail.com>
2023-08-28 21:07:43 +0300

    App.js

--- App.js
+++ App.js
@@ -27,9 +27,19 @@
     }
 
 
-    if(5=5){
-        console.log(öyle)
+
+
+let shape = '';
+for(let i=1;i<=height;i++){
+    let line = ' '.repeat(height - i)
+    if(i===1 || i===height){
+        line += '*'.repeat(2 * i - 1)
     }
-    
+    else{
+        line += '*' + ' '.repeat(2 * i - 3) + '*'
+    }
+    shape += line + '\n'
+}
+document.getElementById('shape').textContent = shape;
     
     

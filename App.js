126e5e46ce8e2e7310530b11a3cad1433996bb96
Mert Önder <mert.ondeeer77@gmail.com>
2023-09-04 23:25:55 +0300

    Javascript example

--- App.js
+++ App.js
@@ -75,5 +75,14 @@ function yildizDeseni(boyut) {
   
   yildizDeseni(5); // Bu örnekte 5 satırlık bir yıldız deseni oluşturuyoruz.
   
-    
+const n = 5; // İstediğiniz satır sayısı
+
+  for (let i = 1; i <= n; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+      row += j + ' ';
+    }
+    console.log(row);
+  }
+  
     
